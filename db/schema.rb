5bcc1ed398f130dbbaa14a78d62e0882c8e3cd03
Hrvoje Kopinc <hrwaaa@gmail.com>
2013-03-23 21:58:39 +0100

    Login with username, add default admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320102845) do
+ActiveRecord::Schema.define(:version => 20130323200911) do
+
+  create_table "probas", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -39,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130320102845) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+  #default admin
+  User.create(username: 'admin', email: 'admin@admin.com', password: 'adminadmin')
