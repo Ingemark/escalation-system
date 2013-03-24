cecda8881931e06862add572b931ee8410303fb4
Hrvoje Kopinc <hrwaaa@gmail.com>
2013-03-24 15:41:48 +0100

    Users model field configuratin

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,28 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation,
     :remember_me
   # attr_accessible :title, :body
+  #
+
+  rails_admin do
+    list do
+      field :id
+      field :username
+      field :email
+    end
+    show do
+      field :username
+      field :email
+      field :sign_in_count
+      field :current_sign_in_at
+      field :last_sign_in_at
+      field :current_sign_in_ip
+      field :last_sign_in_ip
+    end
+    edit do
+      field :username
+      field :email
+      field :password
+      field :password_confirmation
+    end
+  end
 end
