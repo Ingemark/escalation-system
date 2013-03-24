066f657dcaf75b4816c781fd7c246ac7a83f9ace
Hrvoje Kopinc <hrwaaa@gmail.com>
2013-03-24 16:50:40 +0100

    Username and pass confirmation can't be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation,
     :remember_me
-  # attr_accessible :title, :body
-  #
+
+  validates :username, presence: true
+  validates :password_confirmation, presence: true
 
   rails_admin do
     list do
