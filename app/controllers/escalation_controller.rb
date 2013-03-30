8e03cfea60af392fe9c5a582da95b61910558163
Hrvoje Kopinc <hrwaaa@gmail.com>
2013-03-30 23:27:04 +0100

    EscalationController external_reference_id renamed

--- app/controllers/escalation_controller.rb
+++ app/controllers/escalation_controller.rb
@@ -3,10 +3,10 @@ class EscalationController < ApplicationController
 
   # POST /escalation
   def create
-    external_reference_id = params[:escalation][:external_reference_id]
+    external_id = params[:escalation][:external_reference_id]
     context_id = params[:escalation][:context_id]
 
-    if external_reference_id.nil? or context_id.nil?
+    if external_id.nil? or context_id.nil?
       render :json => { :status => :error,
                         :message => 'Request must contain external_reference_id'\
                           ' and context_id'}
@@ -25,12 +25,12 @@ class EscalationController < ApplicationController
         #check if there exists scheduled escalation with same context_id &
         #external_id
         unless ScheduledEscalation.where(subscription_id: sub.id).
-          where(external_reference_id: external_reference_id).
+          where(external_reference_id: external_id).
           where(status: 'scheduled').exists?
           duedate = DateTime.now + level.when.minutes
           if ScheduledEscalation.create(subscription_id: sub.id,
                                         duedate: duedate,
-                                        external_reference_id: external_reference_id,
+                                        external_reference_id: external_id,
                                         status: 'scheduled')
             escal_count += 1
           end
@@ -50,10 +50,10 @@ class EscalationController < ApplicationController
 
   # DELETE /escalation
   def destroy
-    external_reference_id = params[:escalation][:external_reference_id]
+    external_id = params[:escalation][:external_reference_id]
     context_id = params[:escalation][:context_id]
 
-    if external_reference_id.nil? or context_id.nil?
+    if external_id.nil? or context_id.nil?
       render :json => { :status => :error,
                         :message => 'Request must contain external_reference_id'\
                           ' and context_id'}
@@ -65,11 +65,18 @@ class EscalationController < ApplicationController
                         :message => "Context_id is not valid"}
       return
     end
+
+    unless ScheduledEscalation.where(external_reference_id: external_id).exists?
+      render :json => { :status => :error,
+                        :message => "Escalations with that external_reference_id"\
+                          " don't exist" }
+      return
+    end
     
     escal_count = 0
     EscalationLevel.where(context_id: context_id).each do |level|
       Subscription.where(escalation_level_id: level.id).each do |sub|
-        ScheduledEscalation.where(external_reference_id: external_reference_id).
+        ScheduledEscalation.where(external_reference_id: external_id).
           where(status: 'scheduled').each do |escal|
           escal_count += 1
           escal.status = 'canceled'
