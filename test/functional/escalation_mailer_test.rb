ceed43f7933344abe93e37e13be201bc547cbf50
Hrvoje Kopinc <hrvoje.kopinc@gmail.com>
2013-04-16 16:35:30 +0200

    Add EscalationMailer test & update fixtures

--- test/functional/escalation_mailer_test.rb
+++ test/functional/escalation_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class EscalationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'escalation_mail' do
+    escal = scheduled_escalations(:one)
+
+    email = EscalationMailer.escalate(escal).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_equal "Eskalator", email.subject
+    assert_equal ["alen@inge-mark.hr"], email.to
+    assert_match /Ovo je eskalacija/, email.body.encoded
+  end
 end
