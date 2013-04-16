6c0b3b66b94d7173289d8c6e0a6dcdf9af28b0e8
Hrvoje Kopinc <hrvoje.kopinc@gmail.com>
2013-04-16 11:25:07 +0200

    Add SMTP parameters checks in escalate_mail rake

--- app/mailers/escalation_mailer.rb
+++ app/mailers/escalation_mailer.rb
@@ -2,7 +2,6 @@ class EscalationMailer < ActionMailer::Base
   default from: "escalation.system@inge-mark.hr"
 
   def escalate(escalation)
-    init_smtp
     @escalation = escalation
 
     context = escalation.subscription.escalation_level.context
@@ -11,33 +10,11 @@ class EscalationMailer < ActionMailer::Base
     subject = Template.find_by_context_id_and_delivery_service_id_and_field(
       context.id, mail_service.id, 'subject').try(:content) || ''
     body = Template.find_by_context_id_and_delivery_service_id_and_field(
-      context.id, mail_service.id, 'body').try(:content)|| ''
+      context.id, mail_service.id, 'body').try(:content) || ''
 
     mail to: escalation.subscription.delivery_address.address, subject:
       subject do |format|
       format.text  { render :inline => body }
     end
-  end
-
-  private
-  def init_smtp
-    mail_id = DeliveryService.find_by_name('mail').id
-    address = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
-      mail_id, "address").value
-    user_name = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
-      mail_id, "user_name").value
-    password = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
-      mail_id, "password").value
-    port = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
-      mail_id, "port").value
-    authentication = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
-      mail_id, "authentication").value
-
-    ActionMailer::Base.smtp_settings = {
-      address: address,
-      user_name: user_name,
-      password: password,
-      port: port,
-      authentication: authentication }
   end
 end
--- lib/tasks/escalate_mail.rake
+++ lib/tasks/escalate_mail.rake
@@ -2,6 +2,12 @@ namespace :escalate do
   
   desc "Send escalation emails"
   task :mail => :environment do
+    unless init_smtp
+      puts 'Set SMTP parameters: address, user_name, password, port &' \
+        ' authentication.'
+      next
+    end
+
     ScheduledEscalation.where("status = ? and duedate < ?", 'scheduled',
       Time.now).each do |escal|
       delivery_service = escal.subscription.delivery_address.delivery_service
@@ -20,6 +26,31 @@ namespace :escalate do
           puts "Email delivered to: #{delivery_address}"
         end
       end
+    end
+  end
+
+  def init_smtp
+    mail_id = DeliveryService.find_by_name('mail').id
+    begin
+      address = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
+        mail_id, "address").value
+      user_name = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
+        mail_id, "user_name").value
+      password = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
+        mail_id, "password").value
+      port = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
+        mail_id, "port").value
+      authentication = DeliveryServiceProperty.find_by_delivery_service_id_and_key(
+        mail_id, "authentication").value
+    rescue
+      return false
+    else
+      ActionMailer::Base.smtp_settings = {
+        address: address,
+        user_name: user_name,
+        password: password,
+        port: port,
+        authentication: authentication }
     end
   end
 end
