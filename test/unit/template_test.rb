f8613ae8a13b65fcdd8d9b6223b8e46a79bb198d
Hrvoje Kopinc <hrvoje.kopinc@gmail.com>
2013-04-16 16:47:02 +0200

    Add Template unit test

--- test/unit/template_test.rb
+++ test/unit/template_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attributes must not be empty" do
+    template = Template.new
+    assert template.invalid?
+    assert template.errors[:context_id].any?
+    assert template.errors[:delivery_service_id].any?
+    assert template.errors[:field].any?
+    assert template.errors[:content].any?
+  end
+
+  test "field, context & delivery_service must be unique" do
+    template1 = Template.create(context_id: contexts(:inge_mark).id,
+                                delivery_service_id: delivery_services(:mail).id,
+                                field: 'test',
+                                content: 'test',)
+    assert template1.valid?
+
+    template2 = Template.new(context_id: contexts(:inge_mark).id,
+                                delivery_service_id: delivery_services(:mail).id,
+                                field: 'test',
+                                content: 'test',)
+    assert template2.invalid?
+    assert template2.errors[:field].any?
+  end
 end
